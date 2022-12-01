e468a2bb31274ca61ad7ed50f0d860a4436ed1ee
Lanscarlos <lanscarlos@hotmail.com>
2022-12-01 12:31:30 +0800

    init commit

---
+++ src/main/kotlin/top/lanscarlos/poros/PorosArtifact.kt
@@ -0,0 +1,11 @@
+package top.lanscarlos.poros
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object PorosArtifact : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
